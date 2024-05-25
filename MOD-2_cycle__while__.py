229fd43c3a9b685543947b072c7775ea089a1940
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-05-25 11:53:47 +0300

    Add files via upload

--- MOD-2_cycle__while__.py
+++ MOD-2_cycle__while__.py
@@ -37,6 +37,31 @@ while num <= len(my_list):
 print(f'Список положительных чисел: \n{positive_list}')
 
 
+# еще пример
+
+# Сортировка фруктов в два списка
+
+favorite_fruits = []
+not_favorite_fruits = []
+
+while True:
+    fruits = input('Название фрукта: ').lower()  # Приводим ввод к нижнему регистру
+
+    if fruits in ['яблоко', 'груша', 'персик']:
+        print('Вы угадали! Это я люблю ')
+        favorite_fruits.append(fruits)
+        print(f'Список любимых фруктов: \n{favorite_fruits}')
+    else:
+        print('Нет, спасибо - это не вкусно!')
+        not_favorite_fruits.append(fruits)
+        print(f'Список  не любимых фруктов: \n{not_favorite_fruits}')
+
+    continue_game = input("Хотите ввести ещё один фрукт? (y/n)")
+    if continue_game != 'y':
+        break  # Выходим из цикла, если пользователь не хочет продолжать
+
+print(f"Мои нелюбимые фрукты: {not_favorite_fruits}")
+
 
 
 
